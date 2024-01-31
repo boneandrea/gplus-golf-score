b7361bd7cd3c2ee887186c7c5e73e2b02ed57549
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-01 02:52:36 +0900

    handle warning

--- index.php
+++ index.php
@@ -9,9 +9,14 @@ ini_set('xdebug.var_display_max_children', -1);
 ini_set('xdebug.var_display_max_data', -1);
 ini_set('xdebug.var_display_max_depth', -1);
 
+// warning もエラーにする
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    throw new \RuntimeException(sprintf('errno=%s: %s on line %d in file %s', $errno, $errstr, $errline, $errfile));
+});
+
 $x=new TotalScore();
 $r=$x->getIGolf();
-var_dump(json_encode($r,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
+//var_dump(json_encode($r,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
 $r=$x->getMarshalI();
 var_dump(json_encode($r,JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE));
 $x->storeScores();
