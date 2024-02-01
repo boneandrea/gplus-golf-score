4564b4b09fa97c6eafbb5acaef0e7071a73cdfff
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-01 09:29:24 +0900

    PythonでDBアクセスする

---
+++ db.py
@@ -0,0 +1,25 @@
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+class Db:
+    def connect_db(self):
+        return MongoClient(os.environ.get('SERVER'),
+                           int(os.environ.get('PORT')),
+                           username=os.environ['USERNAME'],
+                           password=os.environ['PASSWORD'])
+
+
+x = Db()
+client = x.connect_db()
+db = client["score"]
+score = db["score"]
+
+score.insert_one({"fe": 17, "zuba": [3, 3, 4]})
+
+item = score.find({})
+for i in item:
+    print(i)
