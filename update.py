03a9e7c1595307831f479cc5da9bcce20cc508bd
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-02 03:08:36 +0900

    Update html using template

---
+++ update.py
@@ -0,0 +1,23 @@
+from jinja2 import Environment, FileSystemLoader
+from total import *
+
+# 1. テンプレートファイルの場所と、レンダリング時のtrim設定を行う。
+#    Environmentクラスはjinja2の中心的なクラスで、以下のように
+#    設定を元にTemplateインスタンスを生成する役割を担う。
+env = Environment(
+    loader=FileSystemLoader('templates'),
+    trim_blocks=True
+)
+
+# 2. テンプレートファイルを取得しレンダリングする。
+#    レンダリング結果はファイルに出力する。
+template = env.get_template('index.html')
+x = total()
+print(x.create_html_data())
+
+result = template.render(
+    title='ランキング:水曜ゴルフGP 2024',
+    data=x.create_html_data()
+)
+with open('index.html', 'w') as f:
+    f.write(result)
