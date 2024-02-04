dbb7e10b346d39faff5d2fa01fb1d878f0db3ec3
boneandrea <bone.andrea.anthrax@gmail.com>
2024-02-04 09:45:45 +0900

    サーバからデータを受け取る

--- vue-project/src/components/Table.vue
+++ vue-project/src/components/Table.vue
@@ -1,123 +1,74 @@
 <script setup>
-import { ref } from 'vue'
-const msg="本日のスコア"
-const _members=[
-    {
-        name:"上條栄子",
-        gross: 95
-    },
-    {
-        name:"北村友成",
-        gross: 88
-    },
-    {
-        name:"飯沼茂",
-        gross: 75
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-    {
-        name:"高田由美子",
-        gross: 101
-    },
-]
+ import { ref } from 'vue'
+ const msg="本日のスコア"
+ const members=ref([])
+ const fetchData=()=>{
+     const url="http://localhost:5000/"
+     fetch(url).then((response) => {
+         console.log(response.status)
+         return response.json()
+     }).then((data) => {
+         console.log(data)
+         if(data.status=== "error"){
+             throw new Error(data["reason"])
+         }
+         data["scores"].forEach(e=>{
+             members.value.push(e)
+         })
+     }).catch(e=>{
+         console.error(e)
+         alert(e)
+     })
+ }
+ fetchData()
+ // init
+ members.value.forEach((e,i)=>{
+     members.value[i].point=90-i
+     members.value[i].hdcp=36-i*0.5
+     members.value[i].gross=i+80
+     members.value[i].net=members.value[i].gross-members.value[i].hdcp
+ })
 
-const members=ref(_members)
+ function dragList(e,i){
+     console.log(e,i)
+     console.log(members.value)
+ }
 
-// init
-members.value.forEach((e,i)=>{
-    members.value[i].point=90-i
-    members.value[i].hdcp=36-i*0.5
-    members.value[i].gross=i+80
-    members.value[i].net=members.value[i].gross-members.value[i].hdcp
-})
+ function change(e,i,nearPinIndex){
+     if(nearPinIndex===0) members.value[i].near0=true
+     if(nearPinIndex===1) members.value[i].near1=true
+     if(nearPinIndex===2) members.value[i].near2=true
+     if(nearPinIndex===3) members.value[i].near3=true
+ }
 
-function dragList(e,i){
-    console.log(e,i)
-    console.log(members.value)
-}
+ const dragIndex = ref(null);
 
-function change(e,i,nearPinIndex){
-    if(nearPinIndex===0) members.value[i].near0=true
-    if(nearPinIndex===1) members.value[i].near1=true
-    if(nearPinIndex===2) members.value[i].near2=true
-    if(nearPinIndex===3) members.value[i].near3=true
-}
+ const dragStart = (index) => {
+     console.log('drag start', index)
+     dragIndex.value = index
+ }
 
-const dragIndex = ref(null);
+ const hdcp=(i)=>{
+     console.log(members.value[i].hdcp)
+     members.value[i].net=
+         members.value[i].gross-members.value[i].hdcp
+ }
 
-const dragStart = (index) => {
-    console.log('drag start', index)
-    dragIndex.value = index
-}
+ const dragEnter = (index) => {
+     if (index === dragIndex) return
+     const deleteElement = members.value.splice(dragIndex.value, 1)[0]
+     members.value.splice(index, 0, deleteElement)
+     dragIndex.value = index
+ }
 
-const hdcp=(i)=>{
-    console.log(members.value[i].hdcp)
-    members.value[i].net=
-        members.value[i].gross-members.value[i].hdcp
-}
-
-const dragEnter = (index) => {
-    if (index === dragIndex) return
-    const deleteElement = members.value.splice(dragIndex.value, 1)[0]
-    members.value.splice(index, 0, deleteElement)
-    dragIndex.value = index
-}
-
-function send(){
-    console.log(members.value)
-    if(confirm("送信してよいですか？"))
-        console.log(members.value[3].name)
-    console.log(JSON.stringify(members.value))
-    alert("sent")
-}
-const today=new Date()
+ function send(){
+     console.log(members.value)
+     if(confirm("送信してよいですか？"))
+         console.log(members.value[3].name)
+     console.log(JSON.stringify(members.value))
+     alert("sent")
+ }
+ const today=new Date()
 </script>
 <template>
     <div>
@@ -164,7 +115,7 @@ const today=new Date()
                                :checked="members[index].near1"
                         />
                     </td>
-                    <td>
+                    <td class="col-xs-6">
                         <input type="checkbox"
                                @change="change(e,index,2)"
                                :checked="members[index].near2"
@@ -176,7 +127,7 @@ const today=new Date()
                     </td>
                     <td>{{member.gross}}</td>
                     <td class="col-lg-2">
-                        <input class="form-control"
+                        <input class="form-control col-xs-6"
                                type="number" v-model="member.hdcp" step="0.1"
                                @input="hdcp(index)"/>
                     </td>
@@ -189,29 +140,29 @@ const today=new Date()
 </template>
 
 <style scoped>
-h1 {
-    font-weight: 500;
-    font-size: 2.6rem;
-    position: relative;
-    top: -10px;
-}
+ h1 {
+     font-weight: 500;
+     font-size: 2.6rem;
+     position: relative;
+     top: -10px;
+ }
 
-h3 {
-    font-size: 1.2rem;
-}
+ h3 {
+     font-size: 1.2rem;
+ }
 
-.greetings h1,
-.greetings h3 {
-    text-align: center;
-}
-th,td{ white-space: nowrap; }
-@media (min-width: 1024px) {
-    .greetings h1,
-    .greetings h3 {
-        text-align: left;
-    }
-}
-h1{
-    margin-top:0.5em;
-}
+ .greetings h1,
+ .greetings h3 {
+     text-align: center;
+ }
+ th,td{ white-space: nowrap; }
+ @media (min-width: 1024px) {
+     .greetings h1,
+     .greetings h3 {
+         text-align: left;
+     }
+ }
+ h1{
+     margin-top:0.5em;
+ }
 </style>
