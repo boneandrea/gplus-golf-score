5e019d645a0d9a190be3ec365aff3151969922a9
boneandrea <bone.andrea.anthrax@gmail.com>
2024-12-06 07:09:10 +0900

    fix sort logic by prize

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,7 +5,7 @@
 */
 //
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
@@ -23,7 +23,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     };
 
-    // Shrink the navbar 
+    // Shrink the navbar
     navbarShrink();
 
     // Shrink the navbar when page is scrolled
@@ -50,5 +50,30 @@ window.addEventListener('DOMContentLoaded', event => {
             }
         });
     });
-
 });
+
+const setAppendValue=()=>{
+    document.querySelectorAll("tbody tr").forEach(tr=>{
+        let score=0
+        const prize_td=tr.querySelectorAll("td")[4]
+        const prize=prize_td.innerText
+
+        const holeIneOne=prize.match(/ホールインワン (\d+)/)
+        if(holeIneOne){
+            score+=parseInt(holeIneOne[1])*10000
+        }
+        const albatross=prize.match(/アルバトロス (\d+)/)
+        if(albatross){
+            score+=parseInt(albatross[1])*1000
+        }
+        const eagle=prize.match(/イーグル (\d+)/)
+        if(eagle){
+            score+=parseInt(eagle[1])*100
+        }
+        const birdie=prize.match(/バーディ (\d+)/)
+        if(birdie){
+            score+=parseInt(birdie[1])
+        }
+        prize_td.className=`{sortValue: ${score}}`
+    })
+}
