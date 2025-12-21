88b49318fd062eb59060c4fb3f95932a0fa9c9d3
boneandrea <bone.andrea.anthrax@gmail.com>
2025-12-21 13:48:27 +0900

    pythonが--hideを受け取れるようにphpを変更

--- www/update.php
+++ www/update.php
@@ -2,5 +2,7 @@
 
 // start to update
 
-system("/home/ubuntu/golf/webpage/gplus/publish 2>&1");
+$easy=$_GET['easy'] ?? "";
+if($easy==="easy") $easy="--hide";
 
+system("/home/ubuntu/golf/webpage/gplus/publish $easy 2>&1");
